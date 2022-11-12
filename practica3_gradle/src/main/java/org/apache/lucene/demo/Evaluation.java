b688048feb92e0f3c6f066cf5cfe9285168dd93b
Alberto <735976@unizar.es>
2022-11-13 00:18:30 +0100

    stats individuales

--- practica3_gradle/src/main/java/org/apache/lucene/demo/Evaluation.java
+++ practica3_gradle/src/main/java/org/apache/lucene/demo/Evaluation.java
@@ -19,6 +19,7 @@ package org.apache.lucene.demo;
 
 
 import java.io.*;
+import java.text.DecimalFormat;
 import java.util.*;
 
 /** Index all text files under a directory.
@@ -31,7 +32,7 @@ public class Evaluation {
   private Evaluation() {}
 
   /** Index all text files under a directory. */
-  public static void main(String[] args) throws FileNotFoundException {
+  public static void main(String[] args) throws IOException {
     String usage = """
             Evaluation [-qrels QRELS_FILENAME] [-results RESULTS_FILENAME] [-output OUTPUT_FILENAME]
             """;
@@ -83,22 +84,22 @@ public class Evaluation {
     resultsStats = readResults(qrelsTable, resultsReader);
     //Calcular estadisticas
 //    makeStats(resultsStats, outputFile);
-
-//    createOutput(stats, outputFile);
+    createOutput(resultsStats, outputFile);
   }
 
   static HashMap<String, List<String>> readQRels(BufferedReader qrels) {
     HashMap<String, List<String>> qrelsTable = new HashMap<>();
-
+    List<String> docList = new ArrayList<String>();
     String line = null;
     String oldId = "";
-    List<String> docList = new ArrayList<String>();
+
     try {
       line = qrels.readLine();
       oldId = line.split("\t")[0];
     }catch (IOException e){
       e.printStackTrace();
     }
+
     while(line != null){
       String[] cols = line.split("\t");
       if (!oldId.equals(cols[0])){
@@ -122,20 +123,22 @@ public class Evaluation {
 
   static HashMap<String, HashMap<String, Double>> readResults(HashMap<String, List<String>> qrelsTable, BufferedReader results){
     HashMap<String, HashMap<String, Double>> resultsStats = new HashMap<>();
-
+    HashMap<String, Double> stats;
+    List<Integer> points = new ArrayList<>();
     String line = null;
     String oldId = "";
-    HashMap<String, Double> stats = new HashMap<>();
+    int i = 0;
     int rel = 0;
     int tot = 0;
     int rel10 = 0;
-    List<Integer> points = new ArrayList<>();
+
     try {
       line = results.readLine();
       oldId = line.split("\t")[0];
     }catch (IOException e){
       e.printStackTrace();
     }
+
     while(line != null){
       String[] cols = line.split("\t");
       if(qrelsTable.containsKey(cols[0])) {
@@ -147,9 +150,11 @@ public class Evaluation {
           rel = 0; tot = 0; rel10 = 0;
           resultsStats.put(oldId, oldStats);
           oldId = cols[0];
+          i = 0;
         }
-        tot++;
-        if (qrelsTable.get(oldId).contains(cols[1])) {
+        i++;
+        if(i <= 45) tot++;
+        if (qrelsTable.get(oldId).contains(cols[1]) && i <=45) {
           rel++;
           if (tot <= 10) rel10 = rel;
           points.add(tot);
@@ -163,21 +168,24 @@ public class Evaluation {
     }
     stats = makeStats(rel, tot, qrelsTable.get(oldId).size(), rel10, points);
     resultsStats.put(oldId,stats);
-    System.out.println(resultsStats);
     return resultsStats;
   }
 
   private static HashMap<String, Double> makeStats(int tp, int tpfp, int tpfn, int rel10, List<Integer> puntos) {
     HashMap<String, Double> stats = new HashMap<>();
-    double precision = (double) tp / (double) tpfp;
-    stats.put("precision", precision);
-    double recall = (double) tp / (double) tpfn;
-    stats.put("recall", recall);
-    double f1 = (2.0 * precision * recall) / (precision + recall);
-    stats.put("F1", f1);
-    stats.put("prec@10", (double) rel10 / 10.0);
     List<Double> recalls = new ArrayList<>();
     List<Double> precisions = new ArrayList<>();
+    int count = 0;
+    double ap = 0;
+    double precision = (double) tp / (double) tpfp;
+    double recall = (double) tp / (double) tpfn;
+    double f1 = (2.0 * precision * recall) / (precision + recall);
+
+    stats.put("precision", precision);
+    stats.put("recall", recall);
+    stats.put("F1", f1);
+    stats.put("prec@10", (double) rel10 / 10.0);
+
     for (int i = 1; i <= puntos.size(); i++) {
       double p = (double) i / (double) puntos.get(i - 1);
       precisions.add(p);
@@ -187,14 +195,56 @@ public class Evaluation {
       stats.put("r"+i,r);
     }
 
-    double ap = 0;
-    for (double p : precisions){
-      ap += p;
+    for (double p : precisions) ap += p;
+    stats.put("average_precision", ap / precisions.size());
+
+    for (double i = 0.000; i < 1.000; i += 0.100) {
+      double ip = 0.0;
+      for (int j = 0; j < precisions.size(); j++)
+        if((recalls.get(j) - i) >= 0 && precisions.get(j) > ip)
+          ip = precisions.get(j);
+
+      stats.put("ip"+count, ip);
+      count++;
     }
-    ap = ap / precisions.size();
-    stats.put("average_precision", ap);
 
     return stats;
+  }
+
+  private static void createOutput(HashMap<String, HashMap<String, Double>> resultsStats, OutputStreamWriter outputFile) throws IOException {
+    DecimalFormat df = new DecimalFormat("#.##");
+    StringBuilder stats = new StringBuilder();
+
+    for (String key : resultsStats.keySet()) {
+      int i = 1;
+      int id = 0;
+      stats.append("INFORMATION_NEED ").append(key).append("\n");
+      stats.append("precision ").append(String.format("%,.03f",resultsStats.get(key).get("precision"))).append("\n");
+      stats.append("recall ").append(String.format("%,.03f",resultsStats.get(key).get("recall"))).append("\n");
+      stats.append("F1 ").append(String.format("%,.03f",resultsStats.get(key).get("F1"))).append("\n");
+      stats.append("prec@10 ").append(String.format("%,.03f",resultsStats.get(key).get("prec@10"))).append("\n");
+      stats.append("average_precision ").append(String.format("%,.03f",resultsStats.get(key).get("average_precision"))).append("\n");
+      Double rec = resultsStats.get(key).get("r"+i);
+      stats.append("recall_precision\n");
+      while(rec != null) {
+        String p = String.format("%,.03f", resultsStats.get(key).get("p"+i));
+        String r = String.format("%,.03f", resultsStats.get(key).get("r"+i));
+        stats.append(r).append(" ").append(p).append("\n");
+        i++;
+        rec = resultsStats.get(key).get("r"+i);
+      }
+      stats.append("interpolated_recall_precision\n");
+      for(double j = 0.000; j <= 1.000; j += 0.100) {
+        String p = String.format("%,.03f", resultsStats.get(key).get("ip"+id));
+        stats.append(String.format("%,.03f", j)).append(" ").append(p).append("\n");
+        id++;
+      }
+      stats.append("\n");
+    }
+
+
+    outputFile.write(stats.toString());
+    outputFile.flush();
   }
 
 //  private static void makeStats(HashMap<String, List<Integer>> resultsStats,
